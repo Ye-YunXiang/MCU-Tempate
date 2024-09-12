722d7ab6c8605c08545fa5f8d7a480e510b9c665
YeYunXiang <1198368393@qq.com>
2024-09-12 14:00:54 +0800

    添加了STM32L系列

---
+++ MCU-STM32/STM32L071CBT6工程模板/project/source/main_file_define.h
@@ -0,0 +1,65 @@
+/******************************************************************************
+* ProjectName: 	GSM koolboks battery controler version1 projection
+* FileName: 	main_file_define.h
+* Description:	Manage all header files
+* 
+* Author:		Poetry Cloud
+* Date: 	 	2023/09/25
+* Notes: 
+* -----------------------------------------------------------------
+* Histroy: V1.0 2023/09/15, Cloud create this file
+* CPU:HC32L170JATA  FLASH:64K,128页每页512字节
+******************************************************************************/
+
+// main_file_define.h文件
+#ifndef __MAIN_FILE_DEFINE_H
+#define __MAIN_FILE_DEFINE_H
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <locale.h>
+
+#include "gpio.h"
+#include "ddl.h"
+#include "rtc.h"
+#include "uart.h"
+#include "sysctrl.h"
+#include "flash.h"
+#include "bt.h"
+#include "lptim.h"
+#include "lpuart.h"
+
+#include "system_set_clock.h"
+#include "system_lcd1602.h"
+#include "system_internal_rtc.h"
+#include "system_uart.h"
+#include "system_matrix_key_scan.h"
+#include "system_seve_flash.h"
+#include "system_led_manage.h"
+#include "system_run_manage.h"
+#include "system_gsm_m25.h"
+
+#include "open_paygo_display.h"
+#include "open_paygo_flash.h"
+#include "open_paygo_manage.h"
+#include "open_paygo_time.h"
+
+//#include "opaygo_core.h"
+#include "device_parameters.h"
+//#include "opaygo_decoder.h"
+
+
+
+
+
+
+#endif 
+
+
+
---
+++ MCU-STM32/STM32L071CBT6工程模板/project/source/main_struct_typedef.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_STRUCT_TYPEDEF_H_
+#define __MAIN_STRUCT_TYPEDEF_H_
+
+// system_seve_flash.h
+typedef struct flash_lock __flash_lock; 
+typedef struct flash_date_one __flash_date_one; 
+typedef struct flash_date_tow __flash_date_tow; 
+typedef struct flash_product_time __flash_product_time; 
+typedef struct flash_product_id __flash_product_id; 
+
+// system_internaal_rtc.h
+typedef struct rtc_time rtc_time_t;
+
+
+
+
+
+#endif
