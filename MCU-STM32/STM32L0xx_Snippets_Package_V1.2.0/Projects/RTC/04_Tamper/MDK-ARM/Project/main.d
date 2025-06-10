a59c98776660e184dca7bf5fcb66dfd62b6e4629
YeYunXiang <1198368393@qq.com>
2025-06-10 17:05:44 +0800

    更新HC32L19x

---
+++ MCU-STM32/STM32L0xx_Snippets_Package_V1.2.0/Projects/RTC/04_Tamper/MDK-ARM/Project/main.d
@@ -0,0 +1,9 @@
+project\main.o: ../main.c
+project\main.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h
+project\main.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h
+project\main.o: ../../../../Drivers/CMSIS/Include/core_cm0plus.h
+project\main.o: C:\Software\Programme\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+project\main.o: ../../../../Drivers/CMSIS/Include/core_cmInstr.h
+project\main.o: ../../../../Drivers/CMSIS/Include/core_cmFunc.h
+project\main.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h
+project\main.o: C:\Software\Programme\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
---
+++ MCU-STM32/STM32L0xx_Snippets_Package_V1.2.0/Projects/RTC/04_Tamper/MDK-ARM/Project/system_stm32l0xx.d
@@ -0,0 +1,8 @@
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Source/Templates/system_stm32l0xx.c
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Include/core_cm0plus.h
+project\system_stm32l0xx.o: C:\Software\Programme\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Include/core_cmInstr.h
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Include/core_cmFunc.h
+project\system_stm32l0xx.o: ../../../../Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h
